66a8fa8ce43460808eb68598b48bc71b9a638838
Yovvel <joffrey.nolting@gmail.com>
2021-12-28 15:17:33 +0100

    added git aliases

--- .custom_cmds.sh
+++ .custom_cmds.sh
@@ -1,15 +1,30 @@
 # aliasses
 alias cb="cd .."
 alias c="clear"
+alias h="cd ~
+"
+# Easyer navigation: .., ...,
+alias ..="cd .."
+alias ...="cd ../.."
+
+# git commands
+alias g="git $1"
+alias gs="git status"
+alias gl="git log"
+alias gi="git init $1"
+alias gr="git remote $1"
+alias gf="git fetch $1"
+alias gm="git merge $1"
+alias ga="git add $1"
+alias gaa="git add ."
+alias gcm="git commit -m $1"
+alias gp="git push $1 $2"
 
 
 # functions
-function cdgit(){
+function cgit(){
     clear
     cd ~/Documents/git/
 }
 
-function test(){
-    echo "function works!"
-}
 
