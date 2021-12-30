e5676347d09441f68413c6e16ae3792d8ca41e8d
Yovvel <joffrey.nolting@gmail.com>
2021-12-30 09:36:16 +0100

    modified .custom_cmds.sh

--- .custom_cmds.sh
+++ .custom_cmds.sh
@@ -1,8 +1,14 @@
-# aliasses
-alias cb="cd .."
+# basic bash aliases
+alias h="cd ~"
+alias e="exit"
+alias s="sudo $1"
+alias b="cd .."
 alias c="clear"
-alias h="cd ~
-"
+alias t="touch $1"
+alias md="mkdir $1"
+alias rd="rmdir $1"
+alias nn="nano $1"
+
 # Easyer navigation: .., ...,
 alias ..="cd .."
 alias ...="cd ../.."
@@ -20,11 +26,25 @@ alias gaa="git add ."
 alias gcm="git commit -m $1"
 alias gp="git push $1 $2"
 
+# common folders
+alias doc="cd ~/Documents"
+alias dev="cd ~/Documents/development/"
+alias whome="cd /mnt/c/Users/joffr"
 
-# functions
-function cgit(){
-    clear
-    cd ~/Documents/git/
+# Update system
+alias update="sudo apt-get update && sudo apt-get upgrade -y"
+
+# ------------------------------------------------------------------------- #
+#   Custom commands                                                         #
+# ------------------------------------------------------------------------- #
+
+function mvt(){
+    files="/mnt/c/Users/joffr/Downloads/*.torrent"
+    numbers=0
+    for f in $files
+    do
+        echo "$f found"
+        let "numbers+=1"
+    done
+    echo "found $numbers new books"
 }
-
-
